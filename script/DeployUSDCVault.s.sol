eb8dc1b8b70d547506af3b95156ccd1b932670fa
Lint Action <devs@panoptic.xyz>
2025-12-23 21:53:39 +0000

    Fix code style issues with Prettier

--- script/DeployUSDCVault.s.sol
+++ script/DeployUSDCVault.s.sol
@@ -9,12 +9,10 @@ import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployUSDCVault is Script, DeployHypoVault {
-
     address constant FACTORY_ADDRESS = address(0xebb431ca19A7B245A0827BFA08b5167694B75F38);
     address constant ACCOUNTANT_ADDRESS = address(0x529e41f74221963D43B0b4466674EA24A19F1c27);
     address constant DECODER_ADDRESS = address(0x045AB155Ee70f57fc1672f100FE4939a0d052731);
     address constant AUTHORITY_ADDRESS = address(0xa1FC02EEeDb96F9C0231234DB2824c1FfFeD60CD);
-
 
     address constant VAULT_TURNKEY_ADDRESS = address(0x3c1c79d0cfc316Ba959194c89696a8382d7d283b);
 
